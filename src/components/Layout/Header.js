66cdd529dace9d63bf5cd5c2e14efa3ae3fce176
Anirup Patnaik <anirup049@gmail.com>
2021-05-30 09:36:50 +0530

    Cart component toggle

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from '../Layout/HeaderCartButton';
+import Cart from "../Cart/Cart";
 
 const Header = (props) => {
   return <Fragment>
     <header className={classes.header}>
         <h1>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
     </header>
     <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
